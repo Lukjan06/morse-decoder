e9d913e43719a4f3ad7561d7e0ad43095e1d46f4
Anastasya <elka-mogilev@mail.ru>
2022-03-04 00:04:15 +0300

    feat: add body to the function

--- src/index.js
+++ src/index.js
@@ -38,8 +38,57 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrFromExpr = [];
+    let wordLength = 10;
+    let curStr= ""; // объявляем переменную с пустой строкой
+    let arrDecoded=[]
+    
+    
+    for(let i = 0; i < expr.length; i++) {
+curStr +=expr[i]; //перебираем строку expr посимвольно и добавляем в curStr
+
+if(curStr.length === wordLength) { //когда длина становится необходимой в 10 символовб пушим в arr
+    arrFromExpr.push(curStr);
+    curStr = ""; //обнуляем строки и все заново
+  
 }
+}
+
+console.log(arrFromExpr)
+
+
+let element = arrFromExpr[0];
+console.log(typeof(element))
+
+
+//let smT = '**********';
+
+let arrDec = []
+arrFromExpr.map(function(el) {
+    //if(el === smT) {
+       // arrDec.push('')
+ //   }
+   arrDec.push(el.replace(/^0+/, '').replace(/10/gi, '.').replace(/11/gi,'-'))
+   
+})
+console.log(arrDec)
+
+let keys = Object.keys(MORSE_TABLE);
+console.log(keys);
+
+let arrRes = [];
+ arrDec.map(function(el) {
+     if(Object.keys(MORSE_TABLE).includes(el)) {
+         arrRes.push(MORSE_TABLE[el]);
+     } else{ arrRes.push(' ')}
+})
+console.log(arrRes)
+let result = arrRes.join('');
+console.log(result);
+return result;
+
+}
+
 
 module.exports = {
     decode
